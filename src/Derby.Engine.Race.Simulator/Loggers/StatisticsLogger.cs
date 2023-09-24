2eeac37bfee2b45a8934e511fb04903e2e83ce63
Andreas R. L. Christiansen <AndreasRLC@gmail.com>
2023-09-25 01:49:32 +0200

    Fixed output to be more CSV friendly.

--- src/Derby.Engine.Race.Simulator/Loggers/StatisticsLogger.cs
+++ src/Derby.Engine.Race.Simulator/Loggers/StatisticsLogger.cs
@@ -9,9 +9,11 @@ public class StatisticsLogger
         Console.WriteLine($"Total: {accumulator.Total}");
         Console.WriteLine($"Drawn: {accumulator.DrawnGames}");
 
+        Console.WriteLine("Name,1st,2nd,3rd,4th,5th,<5th,Eliminations,Gallop cards Drawn,Chance cards drawn");
+
         foreach (var result in accumulator.Results)
         {
-            Console.WriteLine($"{result.Key} | 1:{result.Value.FirstPlaces} | 2:{result.Value.SecondPlaces} | 3:{result.Value.ThirdPlaces} | 4:{result.Value.FourthPlaces} | 5:{result.Value.FifthPlaces} | b:{result.Value.BelowFifthPlaces} | e:{result.Value.Eliminations} | g:{result.Value.GallopCardsDrawn} | c:{result.Value.ChanceCardsDrawn}");
+            Console.WriteLine($"{result.Key},{result.Value.FirstPlaces},{result.Value.SecondPlaces},{result.Value.ThirdPlaces},{result.Value.FourthPlaces},{result.Value.FifthPlaces},{result.Value.BelowFifthPlaces},{result.Value.Eliminations},{result.Value.GallopCardsDrawn},{result.Value.ChanceCardsDrawn}");
         }
     }
 }
